f5aa4e78afc68d996ed633c70a9e2cf960edb4a0
LarsdeGraaff <lars.dg@gmail.com>
2015-06-25 12:35:41 +0200

    hello world

--- src/main/java/be/vdab/films/ApplicationConfiguration.java
+++ src/main/java/be/vdab/films/ApplicationConfiguration.java
@@ -1,7 +1,12 @@
 package be.vdab.films;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by jeansmits on 25/06/15.
  */
+@Configuration
+@ComponentScan("be.vdab")
 public class ApplicationConfiguration {
 }
